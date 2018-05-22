cb51ad6e90dfea59c1430af7725f5dfae4277e77
pavlovicnemanja <me@pavlovicnemanja.com>
2018-05-22 16:02:57 +0200

    feat(image-editor): Improvements for image-editor from last session

--- scripts/apps/search/services/MultiImageEditService.js
+++ scripts/apps/search/services/MultiImageEditService.js
@@ -1,41 +1,60 @@
+/* global _ */
+
 MultiImageEditService.$inject = ['$modal', 'authoring'];
 export function MultiImageEditService($modal, authoring) {
     this.edit = (data) => {
         $modal.open({
             templateUrl: 'scripts/apps/search/views/multi-image-edit.html',
             controller: ['$scope', 'config', function ($scope, config) {
-                    $scope.images = _.clone(data);
-
                     $scope.validator = config.validatorMediaMetadata;
 
-                    $scope.placeholders = {};
+                    $scope.origin = angular.copy(data);
+                    $scope.images = angular.copy(data);
 
-                    $scope.metadata = {
-                        headline: compare('headline'),
-                        description_text: compare('description_text'),
-                        archive_description: compare('archive_description'),
-                        alt_text: compare('alt_text'),
-                        byline: compare('byline'),
-                        copyrightholder: compare('copyrightholder'),
-                        usageterms: compare('usageterms'),
-                        copyrightnotice: compare('copyrightnotice')
+                    $scope.placeholder = {};
+                    $scope.isDirty = {};
+
+                    $scope.selectImage = (image) => {
+                        image.unselected = !image.unselected;
+                        image.unselected ?
+                                _.remove($scope.images, (res) => res._id === image._id) :
+                                $scope.images.push(image);
+
+                        return updateMetadata();
+                    };
+
+                    $scope.onBlur = () => {
+                        _.map($scope.origin, (item) => {
+                            if (_.find($scope.images, (image) => image._id === item._id)) {
+                                _.merge(item, _.pick($scope.metadata, _.keys($scope.isDirty)));
+                            }
+                        });
                     };
 
                     $scope.save = (close) => {
-                        let metadata = _.pickBy($scope.metadata, (meta) => {
-                            return meta !== '';
-                        });
-
-                        angular.forEach($scope.images, (image) => {
-                            if (!image.unselected) {
-                                authoring.save(image, metadata);
-                            }
+                        angular.forEach(data, (image) => {
+                            authoring.save(image, _.find($scope.origin, (item) => item._id === image._id));
                         });
 
                         if (close) {
                             $scope.$close();
                         }
                     };
+
+                    updateMetadata();
+
+                    function updateMetadata() {
+                        $scope.metadata = {
+                            headline: compare('headline'),
+                            description_text: compare('description_text'),
+                            archive_description: compare('archive_description'),
+                            alt_text: compare('alt_text'),
+                            byline: compare('byline'),
+                            copyrightholder: compare('copyrightholder'),
+                            usageterms: compare('usageterms'),
+                            copyrightnotice: compare('copyrightnotice')
+                        };
+                    }
 
                     function compare(value) {
                         let uniqueValue = true;
@@ -50,12 +69,12 @@ export function MultiImageEditService($modal, authoring) {
                             return $scope.images[0][value];
                         }
 
-                        $scope.placeholders[value] = '(multiple values)';
+                        $scope.placeholder[value] = '(multiple values)';
 
                         return '';
                     }
-                }],
 
+                }],
             size: 'fullscreen modal--dark-ui'
         });
     };
